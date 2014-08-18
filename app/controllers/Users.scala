926e97f5cbe982e32fc9d1025887a861e00cc2b1
tonilap <tonilap@gmail.com>
2014-08-18 10:44:31 +0200

    Add user controller and list view

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,10 +1,21 @@
 package controllers
 
+import play.api.mvc.Action
+import models.User
+
+
 /**
  * @author tonilap
  * 
  * Users controller
  */
-class Users {
+object Users extends Controller{
 
+  /**
+   * Action to get the list of users  
+   */
+  def list = Action { implicit request =>
+    val users = User.findAll
+    Ok(views.html.activities.list(users))
+  }
 }
